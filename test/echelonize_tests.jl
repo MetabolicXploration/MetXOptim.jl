3691e96cb44e5f498123444f3be66eaaa9f9dacb
josePereiro <jose.pereiro.personal@gmail.com>
2022-11-30 05:12:27 +0100

    free box tests

--- test/echelonize_tests.jl
+++ test/echelonize_tests.jl
@@ -5,11 +5,13 @@ let
     println("."^60)
     println()
 
+    # fba
     net0 = MetXNetHub.pull_net("ecoli_core")
     biom_ider = extras(net0, "BIOM")
     rxns0 = reactions(net0)
     
-    net1 = echelonize(net0)
+    enet = EchelonMetNet(net0)
+    net1 = metnet(enet)
 
     sol0 = fba(net0, TESTS_LINSOLVER)
     biom0 = solution(sol0, biom_ider)
@@ -22,4 +24,25 @@ let
     @test all(isapprox.(flxs0, flxs1; atol = 1e-8))
 
     println()
+
+    # Test free box
+    fvalb0, fvaub0 = fva(net1, TESTS_LINSOLVER; verbose = false)
+
+    idxf, idxd = enet.idxf, enet.idxd
+    Nf, Nd = length(idxf), length(idxd)
+    S = net1.S[:, [idxd; idxf]]
+    G = S[:, (Nd + 1):end]
+    b = net1.b[sortperm(idxd)]
+    
+    lbf, ubf = fvalb0[idxf], fvaub0[idxf]
+    rangef = (ubf .- lbf)
+    @time for t in 1:Int(1e5)
+        vf = (t == 1) ? lbf : # test extreme
+             (t == 2) ? ubf : # test extreme
+                lbf .+ rand(Nf) .* rangef # test random
+        vd = b - G * vf
+        v = [vd; vf]
+        @test isapprox(S * v, b; atol = 1e-8)
+    end
+
 end
